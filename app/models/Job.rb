28d9bb5e64606cc9ba07a7b966e7edfea86e1a46
haris butt <haris@staunch.co>
2024-11-05 11:02:06 +0500

    Updated models for Job Portal

--- app/models/Job.rb
+++ app/models/Job.rb
@@ -2,6 +2,7 @@ class Job < ApplicationRecord
   belongs_to :employer
   has_many :job_applications, dependent: :destroy
 
-  enum display_status: { draft: 0, published: 1, rejected: 2 }
-  enum status: { active: 0, closed: 1 }
+  enum display_status: { draft: 0, published: 1, archived: 2 }
+  enum job_status: { closed: 0, active: 1 }
+  enum job_type: { on_site: 0, hybrid: 1, remote: 2 }
 end
