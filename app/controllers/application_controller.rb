058c457b408a4c1b6e5679648dcfc278f0a05155
haris butt <haris@staunch.co>
2024-11-06 18:00:27 +0500

    Company Registration through devise and redirecting to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,34 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+<<<<<<< HEAD
     company_dashboard_path
   end
 
+=======
+    # Redirect based on user role
+    if resource.role == "company_admin"
+      dashboard_path
+    else
+      dashboard_path
+    end
+  end
+>>>>>>> d4946ac (Company Registration through devise and redirecting to dashboard)
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
 
+<<<<<<< HEAD
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role, company_attributes: [ :name, :industry, :employee_size, :address ] ])
+=======
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role ])
+>>>>>>> d4946ac (Company Registration through devise and redirecting to dashboard)
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
@@ -30,10 +31,33 @@ class RegistrationsController < Devise::RegistrationsController
 
   def verify_email
     render template: "/devise/registrations/verify_email"
+=======
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+    @user.build_company
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      UserMailer.verification_email(@user).deliver_later
+        redirect_to verification_success_path
+    else
+      # Log user errors if saving fails
+      Rails.logger.debug "User errors: #{@user.errors.full_messages}"
+      render :new
+    end
+  end
+
+  def verification_success
+>>>>>>> d4946ac (Company Registration through devise and redirecting to dashboard)
   end
 
   private
 
+<<<<<<< HEAD
   def role_based_redirect(user)
     case user.role
     when "company_admin"
@@ -68,5 +92,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     verify_email_path
+=======
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, :password_confirmation, :role,
+      company_attributes: [ :company_name, :industry, :employee_size, :address ]
+    )
+>>>>>>> d4946ac (Company Registration through devise and redirecting to dashboard)
   end
 end
