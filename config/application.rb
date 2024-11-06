f3e6213c0f800005c05f60aaa4ff106052c643a1
haris butt <haris@staunch.co>
2024-11-06 18:06:01 +0500

    Devise configuration with models and migrations

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require "devise"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +26,7 @@ module JobPortal
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+
 
     # Configuration for the application, engines, and railties goes here.
     #
