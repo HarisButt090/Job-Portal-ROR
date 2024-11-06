96847ffc5cc62a715c5b0e466b78354d724c79c7
haris butt <haris@staunch.co>
2024-11-06 18:00:27 +0500

    Company Registration through devise and redirecting to dashboard

---
+++ app/models/Company.rb
@@ -0,0 +1,11 @@
+class Company < ApplicationRecord
+  # associations
+  belongs_to :user
+  validates :company_name, presence: true
+  validates :industry, presence: true
+  validates :employee_size, presence: true,  numericality: { only_integer: true, greater_than: 0 }
+  validates :address, presence: true
+
+  has_many :employers, dependent: :destroy
+  has_many :jobs, dependent: :destroy
+end
--- app/models/User.rb
+++ app/models/User.rb
@@ -8,13 +8,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+<<<<<<< HEAD
          :recoverable, :rememberable, :validatable
 >>>>>>> a05a5ac (Configured devise)
+=======
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+
+>>>>>>> fcd77f3 (Company Registration through devise and redirecting to dashboard)
   has_one :job_seeker, dependent: :destroy
   has_one :employer, dependent: :destroy
   has_one :company, dependent: :destroy
 
-  # enums
-  enum role: { job_seeker: 0,  company_admin: 1, employer: 2, super_admin: 3 }
+  accepts_nested_attributes_for :company
+
+  enum role: { job_seeker: 0, company_admin: 1, employer: 2, super_admin: 3 }
   enum status: { active: 0, non_active: 1 }
 end
