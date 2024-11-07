76c2fdd4b262d43764e1495f63d5bed22ef9bb03
haris butt <haris@staunch.co>
2024-11-07 14:08:11 +0500

    removed unnnessary code from this branch

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "devise"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,7 +15,6 @@ module Friends
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
 
     # Configuration for the application, engines, and railties goes here.
     #
--- db/migrate/20241105053633_create_users.rb
+++ db/migrate/20241105053633_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration[7.2]
       t.integer :role, default: 0
       t.string :contact_no
       t.integer :status, default: 0
+
       t.timestamps
     end
   end
