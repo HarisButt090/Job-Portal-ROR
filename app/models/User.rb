a2a1842c97b112130430e0d478bb05cf2615d189
haris butt <haris@staunch.co>
2024-11-07 12:47:19 +0500

    Updated models

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :job_seeker, dependent: :destroy
   has_one :employer, dependent: :destroy
-  belongs_to :company, optional: true
+  has_one :company, dependent: :destroy
 
   enum role: { job_seeker: 0,  company_admin: 1, employer: 2, super_admin: 3 }
   enum status: { active: 0, non_active: 1 }
