f2624031f00bae0b660c97192c3355757c4a842d
haris butt <haris@staunch.co>
2024-11-07 19:29:29 +0500

    Removed unnecessary files from this branch

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,17 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :recoverable, :rememberable, :validatable
-=======
-         :recoverable, :rememberable, :validatable,
-         :confirmable
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 8 }
-
->>>>>>> ee28dd6 (Company Registration through devise and redirecting to dashboard)
   has_one :job_seeker, dependent: :destroy
   has_one :employer, dependent: :destroy
   has_one :company, dependent: :destroy
