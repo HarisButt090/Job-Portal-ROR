3e5d83f5c18b5b1a4d1d541230e05bf18266260e
haris butt <haris@staunch.co>
2024-11-07 14:08:11 +0500

    removed unnnessary code from this branch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,12 @@
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment, your application's code is reloaded any time
+  # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.reload_classes_only_on_change = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -15,13 +17,10 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
-<<<<<<< HEAD
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
-=======
-  # Enable/disable caching. By default, caching is disabled.
->>>>>>> 8a6f96b (UPdated the migrations for the job portal project)
+
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
@@ -31,24 +30,21 @@ Rails.application.configure do
     config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
+
     config.cache_store = :null_store
   end
 
-# Action Mailer Configuration
-config.action_mailer.delivery_method = :letter_opener_web
-config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
 
-# config.middleware.use LetterOpenerWeb::Engine, only: :development
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
-# Store uploaded files on the local file system (see config/storage.yml for options).
-config.active_storage.service = :local
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
+  config.action_mailer.perform_caching = false
 
-
-# Disable caching for Action Mailer templates even if Action Controller
-# caching is enabled.
-config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
