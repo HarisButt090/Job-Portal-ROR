5a5c3e0f017467316ccd5194e9707dcf2743e11d
haris butt <haris@staunch.co>
2024-11-07 12:20:24 +0500

    Updated migrations

--- db/migrate/20241105053633_create_users.rb
+++ db/migrate/20241105053633_create_users.rb
@@ -6,12 +6,7 @@ class CreateUsers < ActiveRecord::Migration[7.2]
       t.integer :role, default: 0
       t.string :contact_no
       t.integer :status, default: 0
-<<<<<<< HEAD:db/migrate/20241105053633_create_users.rb
-      t.references :company, null: false, foreign_key: true
-
-=======
       t.references :company, null: true, foreign_key: true
->>>>>>> 04357b1 (Resolved conflicts and added to git ignore):db/migrate/20241105083850_create_users.rb
       t.timestamps
     end
   end
