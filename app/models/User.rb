c67b852c098dd446b3abbdb2395216d309252c54
haris butt <haris@staunch.co>
2024-11-08 11:50:39 +0500

    Removed unnecessary code from this branch

--- app/models/User.rb
+++ app/models/User.rb
@@ -3,12 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_one :job_seeker, dependent: :destroy
   has_one :employer, dependent: :destroy
   has_one :company, dependent: :destroy
-
-  accepts_nested_attributes_for :company
 
   enum role: { job_seeker: 0, company_admin: 1, employer: 2, super_admin: 3 }
   enum status: { active: 0, non_active: 1 }
--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative "boot"
 require "rails/all"
 require "devise"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -17,7 +16,6 @@ module Friends
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
 
     # Configuration for the application, engines, and railties goes here.
     #
