cbf1207e7645f164b5e0054a1d1a21cf9d866fb6
haris butt <haris@staunch.co>
2024-11-08 12:56:37 +0500

    Removed vendor folder

--- vendor/bundle/ruby/3.3.0/gems/letter_opener_web-1.1.3/config/routes.rb
+++
@@ -1,6 +0,0 @@
-LetterOpenerWeb::Engine.routes.draw do
-  delete 'clear'                 => 'letters#clear', :as => :clear_letters
-  get    '/'                     => 'letters#index', :as => :letters
-  get    ':id(/:style)'          => 'letters#show',  :as => :letter
-  get    ':id/attachments/:file' => 'letters#attachment'
-end
