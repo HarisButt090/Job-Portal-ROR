57172cb6cd7d5304eb28cc27e26933164209ad19
haris butt <haris@staunch.co>
2024-11-13 16:38:14 +0500

    Company registration using namespaces

--- db/schema.rb
+++ db/schema.rb
@@ -179,8 +179,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_103102) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+<<<<<<< HEAD
 >>>>>>> f82942d (Company Registration through devise and redirecting to dashboard)
     t.index ["email"], name: "index_users_on_email", unique: true
+=======
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+>>>>>>> 9022dbc (Company registration using namespaces)
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
