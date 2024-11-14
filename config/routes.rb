094c7450f3b3ded3894c47dd0b9b710c93719f97
haris butt <haris@staunch.co>
2024-11-14 12:37:29 +0500

    Rebased with main and resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-<<<<<<< HEAD
-  # Letter opener route
-=======
   root "pages#landing"
 
   devise_for :users, controllers: { registrations: "registrations", sessions: "devise/sessions" }
@@ -18,18 +13,10 @@ Rails.application.routes.draw do
     get "dashboard", to: "dashboard#index", as: "dashboard"
   end
 
->>>>>>> fcd77f3 (Company Registration through devise and redirecting to dashboard)
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-<<<<<<< HEAD
-=======
-  resources :friends
-  get "home/about"
-  root "home#index"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
->>>>>>> a05a5ac (Configured devise)
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -38,6 +25,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-=======
->>>>>>> 3208549 (Company registration using namespaces)
 end
