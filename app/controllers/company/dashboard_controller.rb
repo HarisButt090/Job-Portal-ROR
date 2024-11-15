3c915b57d0b105dedb246a32d73a07f24852a4d9
haris butt <haris@staunch.co>
2024-11-15 17:55:55 +0500

    Used layouts in namespace of company

--- app/controllers/company/dashboard_controller.rb
+++ app/controllers/company/dashboard_controller.rb
@@ -2,6 +2,7 @@
   class Company::DashboardController < ApplicationController
     before_action :authenticate_user!
 
+    layout "company"
     def index
       # Dashboard logic will go here
     end
