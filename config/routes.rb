0107ca45b1c954da77226f18a09c22b2fbf82e98
haris butt <haris@staunch.co>
2024-11-15 11:42:54 +0500

    admin login and redireting to dashbaord

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations", sessions: "devise/sessions" }
 
   devise_scope :user do
-<<<<<<< HEAD
     get "verify_email", to: "registrations#verify_email", as: "verify_email"
     get "company_details", to: "registrations#company_details", as: "company_details"
     post "save_company_details", to: "registrations#save_company_details", as: "save_company_details"
     get "job_seeker_details", to: "registrations#job_seeker_details", as: "job_seeker_details"
     post "save_job_seeker_details", to: "registrations#save_job_seeker_details", as: "save_job_seeker_details"
-
-  namespace :job_seeker do
-    get "dashboard", to: "dashboard#index", as: "dashboard"
-=======
-    get "register/company_admin", to: "registrations#new_company_admin", as: "new_company_admin_registration"
-    get "register/job_seeker", to: "registrations#new_job_seeker", as: "new_job_seeker_registration"
-    post "register", to: "registrations#create", as: "create_registration"
-    get "verification_success", to: "registrations#verification_success", as: "verification_success"
->>>>>>> ea09662 (Registered job seeker and redirected to dashboard)
-  end
 
   namespace :job_seeker do
     get "dashboard", to: "dashboard#index", as: "dashboard"
