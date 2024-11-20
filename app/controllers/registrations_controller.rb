d75771600595ec928746bba21007d3a693e76d7f
HarisButt090 <haris@staunch.co>
2024-11-20 17:42:59 +0500

    Updated company registration witha  new flow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,37 +2,49 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    if params[:user][:role] == "company_admin"
-      resource.build_company(company_params)
-    end
-
     if resource.save
-      handle_successful_signup(resource)
+      role_based_redirect(resource)
     else
       handle_failed_signup(resource)
     end
   end
 
-  def role_details
-    if params[:role] == "company_admin"
-      render partial: "users/registrations/company_details", locals: { f: User.new }
+  def company_details
+    @company = Company.new
+    @user = User.find(params[:user_id])
+  end
+
+  def save_company_details
+    user = User.find(params[:user_id])
+    @company = Company.new(company_params)
+    @company.user_id = user.id
+
+    if @company.save
+      redirect_to verify_email_path, notice: "Company details saved successfully!"
     else
-      render html: ""
+      render :company_details, alert: "There was an error saving company details."
     end
   end
 
   def verify_email
-    render template: "users/registrations/verify_email"
+    render template: "/devise/registrations/verify_email"
   end
 
   private
 
-  def handle_successful_signup(resource)
-    message = resource.active_for_authentication? ? :signed_up : :"signed_up_but_#{resource.inactive_message}"
-    set_flash_message! :notice, message
-    sign_up(resource_name, resource) if resource.active_for_authentication?
-    expire_data_after_sign_in! unless resource.active_for_authentication?
-    redirect_to verify_email_path(format: :html)
+  def role_based_redirect(user)
+    case user.role
+    when "company_admin"
+      redirect_to company_details_path(user_id: user.id)
+    else
+      handle_default_redirect
+    end
+  end
+
+  def handle_default_redirect
+    set_flash_message! :notice, :signed_up
+    sign_up(resource_name, resource)
+    redirect_to root_path
   end
 
   def handle_failed_signup(resource)
@@ -43,12 +55,11 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :role, company_attributes: [ :name, :industry, :employee_size, :address ])
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :role)
   end
 
-
   def company_params
-    params.require(:user).permit(company_attributes: [ :name, :industry, :employee_size, :address ])[:company_attributes]
+    params.require(:company).permit(:name, :industry, :employee_size, :address)
   end
 
   protected
