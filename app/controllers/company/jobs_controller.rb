4052ff72503f2d7221bf490ceba23229635372bf
HarisButt090 <haris@staunch.co>
2024-11-21 18:50:23 +0500

    Closed a job form published jobs and moved to archive and reopened again

--- app/controllers/company/jobs_controller.rb
+++ app/controllers/company/jobs_controller.rb
@@ -1,6 +1,6 @@
 class Company::JobsController < ApplicationController
   before_action :set_company
-  before_action :set_job, only: [:show, :edit, :update, :destroy, :publish]
+  before_action :set_job, only: [:show, :edit, :update, :destroy, :publish, :close, :open]
   layout "company"
 
   def new
@@ -20,7 +20,7 @@ class Company::JobsController < ApplicationController
   end
 
   def drafts
-    @draft_jobs = @company.jobs.where(displayed_status: 0)
+    @draft_jobs = @company.jobs.where(displayed_status: 0, job_status: 0)
   end
 
   def show
@@ -56,9 +56,32 @@ class Company::JobsController < ApplicationController
   end
  
   def manage
-    @active_jobs = @company.jobs.where(displayed_status: 1)
+    @active_jobs = @company.jobs.where(displayed_status: 1, job_status: 1)
   end
   
+  def close
+    
+    if @job.update(displayed_status: 2, job_status: 0)
+      redirect_to manage_company_jobs_path, notice: 'Job was successfully closed.'
+    else
+      redirect_to manage_company_jobs_path, alert: 'Failed to close the job.'
+    end
+  end
+ 
+  def archived
+    @archived_jobs = @company.jobs.where(displayed_status: 2 , job_status: 0)
+  end
+
+  def open
+
+    if @job.update(displayed_status: 1, job_status: 1)
+      redirect_to archived_company_jobs_path, notice: 'Job was successfully reopened and moved to active jobs.'
+    else
+      redirect_to archived_company_jobs_path, alert: 'Failed to reopen the job. Please try again.'
+    end
+  end
+  
+
   private
 
   def set_company
