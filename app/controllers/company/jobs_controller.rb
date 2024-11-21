1d051cd373ad90c8f098cb66d3deb44b2ff3bc64
HarisButt090 <haris@staunch.co>
2024-11-21 16:48:46 +0500

    Edit, Updated and Deleted a job in drafts

--- app/controllers/company/jobs_controller.rb
+++ app/controllers/company/jobs_controller.rb
@@ -1,8 +1,8 @@
 class Company::JobsController < ApplicationController
   before_action :set_company
-  before_action :set_job, only: [:show]
-   layout "company"
-   
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  layout "company"
+
   def new
     @job = @company.jobs.new
   end
@@ -24,18 +24,40 @@ class Company::JobsController < ApplicationController
   end
 
   def show
-    @job
   end
 
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to drafts_company_jobs_path, notice: 'Job was successfully updated.'
+    else
+      flash.now[:alert] = 'Unable to update job. Please check the errors below.'
+      render :edit
+    end
+  end
+
+  def destroy
+    Rails.logger.debug "Destroy action triggered for job ID: #{@job.id}"
+    if @job.destroy
+      redirect_to drafts_company_jobs_path, notice: 'Job was successfully deleted.'
+    else
+      redirect_to drafts_company_jobs_path, alert: 'Failed to delete the job.'
+    end
+  end
   
+
   private
 
   def set_company
     @company = Company.find_by(user_id: current_user.id)
+    redirect_to root_path, alert: "Company not found" unless @company
   end
 
   def set_job
-    @job = @company.jobs.find(params[:id])
+    @job = @company.jobs.find_by(id: params[:id])
+    redirect_to drafts_company_jobs_path, alert: 'Job not found or does not belong to your company.' unless @job
   end
 
   def job_params
