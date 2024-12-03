01c223187df1d15050abd6c739fe357d24024169
HarisButt090 <haris@staunch.co>
2024-12-03 18:51:39 +0500

    Authorize user to dahsbaord

--- app/controllers/company/dashboard_controller.rb
+++ app/controllers/company/dashboard_controller.rb
@@ -1,9 +1,17 @@
+class Company::DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_company_admin
 
-  class Company::DashboardController < ApplicationController
-    before_action :authenticate_user!
+  layout "company"
 
-    layout "company"
-    def index
-      # Dashboard logic will go here
+  def index
+  end
+
+  private
+
+  def authorize_company_admin
+    unless current_user.role == 'company_admin'
+      redirect_to root_path, alert: "Access denied. Only company admins can access the company dashboard."
     end
   end
+end
