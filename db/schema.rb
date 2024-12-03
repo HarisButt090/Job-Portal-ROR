0de5e592c74045ddc416140f8c6a5c3365a4a3be
HarisButt090 <haris@staunch.co>
2024-12-03 12:54:34 +0500

    Remove file from remote but keep locally

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_07_115452) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_054931) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "industry"
@@ -136,11 +136,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_07_115452) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "companies", "users"
