a567b3e0352441d9d5edb77a97cd2006d19a81fe
HarisButt090 <haris@staunch.co>
2024-12-03 11:28:11 +0500

    Added pagination to job seeker dashboard

--- app/controllers/job_seeker/dashboard_controller.rb
+++ app/controllers/job_seeker/dashboard_controller.rb
@@ -4,6 +4,6 @@
     layout "job_seeker"
   
     def index
-      @jobs = Job.where(displayed_status: 'published')
-    end
+      @jobs = Job.where(displayed_status: 'published').page(params[:page]).per(3)
+    end    
   end
