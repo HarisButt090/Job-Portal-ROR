d61b8bcd3af27298eed529865279b61d0d33d38a
HarisButt090 <haris@staunch.co>
2024-12-03 12:54:34 +0500

    Remove file from remote but keep locally

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_07_115452) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_054931) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "industry"
