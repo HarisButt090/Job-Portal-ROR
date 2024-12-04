dda66ba9101d84cc1255bbc0493498e617e8dc21
HarisButt090 <haris@staunch.co>
2024-12-04 14:28:00 +0500

    Removed callbacks from user

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   root "pages#landing"
 
-  devise_for :users, controllers: { registrations: "registrations", sessions: "devise/sessions" }
 
   devise_scope :user do
     get "verify_email", to: "registrations#verify_email", as: "verify_email"
