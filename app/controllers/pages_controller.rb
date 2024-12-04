8b8ac57a654cc8767323bbdd804058e5be084171
HarisButt090 <haris@staunch.co>
2024-12-04 12:38:39 +0500

    Redirection issue of logged inuser

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
   def landing
-  end
-
-  def register
+    if user_signed_in?
+      case current_user.role
+      when "company_admin"
+        redirect_to company_dashboard_path
+      when "job_seeker"
+        redirect_to job_seeker_dashboard_path
+      when "employer"
+        redirect_to employer_dashboard_path
+      when "super_admin"
+        redirect_to admin_dashboard_path
+      else
+        redirect_to root_path, alert: "Invalid role or access!"
+      end
+    end
   end
 end
