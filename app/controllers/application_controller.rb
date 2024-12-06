bf8d692d5d533bbac00a775930002a2b8bdbc16e
HarisButt090 <haris@staunch.co>
2024-12-06 11:31:01 +0500

    Removed unnecessary files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role, company_attributes: [ :name, :industry, :employee_size, :address ] ])
-
+  end
 end
