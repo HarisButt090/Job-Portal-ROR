eac3fe354eaa984429f8f1f7e13fd7762a9f8286
HarisButt090 <haris@staunch.co>
2024-12-16 11:46:39 +0500

    Updated login mutation:

--- app/graphql/mutations/user_login.rb
+++ app/graphql/mutations/user_login.rb
@@ -1,32 +1,41 @@
 class Mutations::UserLogin < Mutations::BaseMutation
-    argument :login_attributes, Types::LoginAttributes, required: true
-    
-    # Returning token, message, and user fields
-    field :token, String, null: true
-    field :message, String, null: true
-    field :user, Types::User::UserType, null: true
-    
-    def resolve(login_attributes:)
-      email = login_attributes[:email]
-      password = login_attributes[:password]
+  argument :login_attributes, Types::LoginAttributes, required: true
+
+  # Returning token, message, and user fields
+  field :token, String, null: true
+  field :message, String, null: true
+  field :user, Types::User::UserType, null: true
+
+  def resolve(login_attributes:)
+    email = login_attributes[:email]
+    password = login_attributes[:password]
   
-      user = User.find_by(email: email)
+    user = User.find_by(email: email)
   
-      if user&.valid_password?(password)
-        token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
-  
-        {
-          token: token,
-          message: 'Login successful',
-          user: user
-        }
-      else
-        {
-          token: nil,
-          message: 'Invalid email or password',
-          user: nil
-        }
-      end
+    if user&.valid_password?(password)
+      jti = SecureRandom.uuid
+      exp = 24.hours.from_now.to_i 
+
+      payload = {
+        sub: user.id,
+        name: user.name,
+        jti: jti,
+        exp: exp
+      }
+      token = JWT.encode(payload, Rails.application.secret_key_base, 'HS256')
+
+      {
+        token: token,
+        message: 'Login successful',
+        user: user
+      }
+    else
+      {
+        token: nil,
+        message: 'Invalid email or password',
+        user: nil
+      }
     end
   end
   
+end
