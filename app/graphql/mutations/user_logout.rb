d520cd2f0af572012fc9b7f952adbc2455fb07c3
HarisButt090 <haris@staunch.co>
2024-12-16 11:49:49 +0500

    Updated logout mutation

--- app/graphql/mutations/user_logout.rb
+++ app/graphql/mutations/user_logout.rb
@@ -2,37 +2,18 @@ class Mutations::UserLogout < Mutations::BaseMutation
   field :message, String, null: false
 
   def resolve
-    # Check if the user is authenticated
     raise GraphQL::ExecutionError, "Not authenticated" if context[:current_user].nil?
 
-    # Extract the Authorization header
     header = context[:request].headers['Authorization']
-    binding.pry
     token = header&.split(' ')&.last
-    binding.pry
-
-    # Handle missing token
     raise GraphQL::ExecutionError, "No token provided" if token.nil?
 
     begin
-      # Decode the JWT token
       payload = JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256').first
-      binding.pry
-
-      # Extract the `jti` (JWT ID) from the payload
       jti = payload['jti']
-      binding.pry
-
       exp = payload['exp']
-      binding.pry
-
-      # Ensure `exp` is present and valid
-      raise GraphQL::ExecutionError, "Invalid token payload: missing 'exp'" if exp.nil?
-
-      # Create an entry in the JwtDenylist
       JwtDenylist.create!(jti: jti, exp: Time.at(exp))
 
-      # Return a successful logout message
       { message: 'Logout successful' }
     rescue JWT::DecodeError
       raise GraphQL::ExecutionError, "Invalid token"
