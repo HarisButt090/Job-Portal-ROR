e4f96504669c1367827d124d5230878b98ba2699
HarisButt090 <haris@staunch.co>
2024-12-17 10:34:12 +0500

    Fetched all recruiters and by id

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,6 @@ module Types
   class QueryType < Types::BaseObject
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
+    field :recruiters, resolver: Resolvers::RecruitersResolver
   end
 end
